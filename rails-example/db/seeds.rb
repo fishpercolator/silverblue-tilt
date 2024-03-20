7890f585965c301ba3c13b97f6da17d8109f8a6e
Quinn Daley <quinn@fishpercolator.co.uk>
2024-03-20 15:28:46 +0000

    Fresh install of a Rails app using 'rails new --javascript=esbuild --css=bulma -d postgresql rails-example'

--- rails-example/db/seeds.rb
+++ rails-example/db/seeds.rb
@@ -1,6 +1,9 @@
-Book.create! [
-  {title: 'Emma', author: 'Jane Austen'},
-  {title: 'Nineteen Eighty-Four', author: 'George Orwell'},
-  {title: 'Twin Peaks: The Secret History', author: 'Mark Frost'},
-  {title: 'Wuthering Heights', author: 'Emily Bronte'}
-]
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
